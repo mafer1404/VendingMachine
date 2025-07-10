1290cb3258a799346d46ea7015d0547c8d6c2b12
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-07-09 20:04:20 -0600

    Actualizar programa principal para establecer la ruta de la pagina

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import apiPlugin from './plugins/api';
+import HomePage from "./components/VendingMachine.vue";
 
-createApp(App).mount('#app')
+import '@/assets/css/VendingMachine.css'
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", name: "HomePage", component: HomePage },
+  ],
+});
+
+const app = createApp(HomePage);
+app.use(router);
+app.use(apiPlugin);
+app.mount("#app");
