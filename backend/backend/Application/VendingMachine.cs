f9d5c76ed46aec51a558b28888600a98e17d5a27
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-07-09 20:00:50 -0600

    Aplicacion para simular la cantidad de bebidas dentro de la maquina

---
+++ backend/backend/Application/VendingMachine.cs
@@ -0,0 +1,22 @@
+﻿using backend.Domain;
+namespace backend.Application
+{
+  public class VendingMachine
+  {
+    private List<DrinkModel> _drinks;
+
+    public VendingMachine()
+    {
+      _drinks = new List<DrinkModel>
+        {
+            new DrinkModel { Name = "Sprite", Quantity = 15, Price = 975 },
+            new DrinkModel { Name = "Fanta", Quantity = 10, Price = 950 },
+            new DrinkModel { Name = "Pepsi", Quantity = 8, Price = 750 },
+            new DrinkModel { Name = "Coca Cola", Quantity = 10, Price = 800 },
+        };
+    }
+
+    public List<DrinkModel> GetAllDrinks() => _drinks;
+
+  }
+}
