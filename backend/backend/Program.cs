ca33a21a9278e77603a6a4c9813462059302d94e
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-07-09 20:09:05 -0600

    Agregar como servicio el VendingMachine y agregar conexion al front end

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,4 +1,20 @@
+using backend.Application;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy(name: MyAllowSpecificOrigins,
+      policy =>
+      {
+        policy
+              .WithOrigins("http://localhost:8080")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+      });
+});
 
 // Add services to the container.
 
@@ -6,19 +22,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<VendingMachine>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllers();
 
