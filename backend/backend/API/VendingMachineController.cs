90f1e33736f27d0c1664f6a4e435550d060c3bed
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-07-09 20:00:16 -0600

    Controlador de la maquina expendedora, crear metodo para obtener cantidad de bebidas

---
+++ backend/backend/API/VendingMachineController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using backend.Domain;
+using backend.Application;
+namespace backend.API.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class VendingMachineController : ControllerBase
+  {
+    private readonly VendingMachine _vendingMachineService;
+
+    public VendingMachineController(VendingMachine vendingMachineService)
+    {
+      _vendingMachineService = vendingMachineService;
+    }
+
+    [HttpGet("drinks")]
+    public ActionResult<List<DrinkModel>> GetDrinks()
+    {
+      return Ok(_vendingMachineService.GetAllDrinks());
+    }
+  }
+}
