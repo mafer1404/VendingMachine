4d1f903d6157531c8ef48196002dbe4d6a1206d4
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-07-10 19:12:07 -0600

    Componente para mostrar el desglose del vuelto

---
+++ frontend/src/components/ChangeBreakdown.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="breakdown && Object.keys(breakdown).length" class="alert w-100" style="background-color: #FEB2B4;">
+    <h5>Desglose:</h5>
+    <ul>
+      <li v-for="(qty, coin) in breakdown" :key="coin">
+        {{ qty }} de {{ coin }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    breakdown: Object
+  }
+}
+</script>
