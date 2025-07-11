c1712a065f05a595a2cf50bceddc5dc35a578822
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-07-10 19:04:52 -0600

    Agregar metodo para comprar una bebida

--- backend/backend/Application/VendingMachine.cs
+++ backend/backend/Application/VendingMachine.cs
@@ -1,5 +1,6 @@
 ﻿using backend.Domain;
-namespace backend.Application
+
+namespace backend.Domain
 {
   public class VendingMachine
   {
@@ -12,11 +13,19 @@ namespace backend.Application
             new DrinkModel { Name = "Sprite", Quantity = 15, Price = 975 },
             new DrinkModel { Name = "Fanta", Quantity = 10, Price = 950 },
             new DrinkModel { Name = "Pepsi", Quantity = 8, Price = 750 },
-            new DrinkModel { Name = "Coca Cola", Quantity = 10, Price = 800 },
+            new DrinkModel { Name = "Coca Cola", Quantity = 10, Price = 800 }
         };
     }
 
     public List<DrinkModel> GetAllDrinks() => _drinks;
 
+    public bool BuyDrink(string name, int quantity)
+    {
+      var drink = _drinks.FirstOrDefault(d => d.Name == name);
+      if (drink == null || drink.Quantity < quantity) return false;
+
+      drink.Quantity -= quantity;
+      return true;
+    }
   }
 }
