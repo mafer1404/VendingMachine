d332032d2c4c0b2e7fb2b4fd84b425cbb3554db0
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-07-10 19:06:44 -0600

    Componente que muestra el resumen de una compra

---
+++ frontend/src/components/PurchaseResult.vue
@@ -0,0 +1,37 @@
+<template>
+  <h2 class="text-center mb-4">Resumen de compra</h2>
+  <div v-if="total > 0">
+    <div class="card shadow-sm mx-auto w-100" style="max-width: 400px;">
+      <div class="card-body" style="background-color: #FEB2B4;">
+        <p>Total a pagar: <strong>₡{{ total }}</strong></p>
+        <p>Dinero ingresado: <strong>₡{{ userMoney }}</strong></p>
+
+        <div v-if="userMoney >= total" class="mt-3">
+          <p>Vuelto: <strong>₡{{ userMoney - total }}</strong></p>
+        </div>
+
+        <div v-else class="text-danger mt-3">
+          Dinero insuficiente.
+        </div>
+      </div>
+    </div>
+
+    <button
+      class="btn btn-primary mx-auto d-block"
+      style="background-color: #EE8187; border-color: #d65a60; color: black;
+      margin-top: 15px; max-width: 400px; width: 100%;"
+      @click="$emit('purchase')">
+      Confirmar compra
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    total: Number,
+    userMoney: Number,
+    selectedQuantities: Object
+  }
+}
+</script>
