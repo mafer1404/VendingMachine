fe4ab3d563a59db86634b5ec8785779a78416412
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-07-10 19:12:31 -0600

    Componente para mostrar mensaje de exito de la compra

---
+++ frontend/src/components/SuccessBanner.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-if="message" class="alert w-100" style="background-color: #FEB2B4; margin-top: 10px;">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
