68a692352ac069ff1383387ec62c8f0b61d8056d
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-07-10 19:05:42 -0600

    Modelo de datos para manejar la compra

---
+++ backend/backend/Domain/PurchaseRequestModel.cs
@@ -0,0 +1,15 @@
+﻿namespace backend.Domain
+{
+  public class PurchaseItemModel
+  {
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+  }
+
+  public class PurchaseRequestModel
+  {
+    public List<PurchaseItemModel> Items { get; set; }
+    public int AmountPaid { get; set; }
+
+  }
+}
