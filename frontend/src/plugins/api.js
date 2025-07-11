637b6e0186a54b39f3b9ff48d25f652bdcccd11d
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-07-10 19:07:18 -0600

    Agregar ruta del api para comprar bebidas

--- frontend/src/plugins/api.js
+++ frontend/src/plugins/api.js
@@ -2,15 +2,19 @@ import axios from "axios";
 
 export default {
   install(app) {
-    const apiBaseURL = "https://localhost:7295/api";
+    const apiBaseURL = "https://localhost:7295/";
 
     app.config.globalProperties.$apiBaseURL = apiBaseURL;
 
     app.config.globalProperties.$api = {
 
       getAvailableDrinks() {
-        return axios.get(`${apiBaseURL}/vendingmachine/drinks`);
+        return axios.get(`${apiBaseURL}VendingMachine/Drinks`);
       },
+
+      buyDrink(purchase) {
+        return axios.post(`${apiBaseURL}VendingMachine/Buy`, purchase)
+    }
     };
   },
 };
