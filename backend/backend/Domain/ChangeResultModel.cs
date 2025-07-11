88e5e6a43aa879323b07319fe271843f4c6927c5
mafer <maria.herreragutierrez@ucr.ac.cr>
2025-07-10 18:59:34 -0600

    Modelo para el calculo del vuelto

---
+++ backend/backend/Domain/ChangeResultModel.cs
@@ -0,0 +1,10 @@
+﻿namespace backend.Domain
+{
+  public class ChangeResultModel
+  {
+    public bool Success { get; set; }
+    public int ChangeAmount { get; set; }
+    public Dictionary<int, int> Breakdown { get; set; }
+    public string Message { get; set; }
+  }
+}
